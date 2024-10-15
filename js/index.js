5978cdea0f0b9b8f3c2d3fa0f1bfeb7bd3141759
josefacf <josefa.casado@uc.cl>
2024-10-14 21:45:58 -0300

    creo que última actualización

--- js/index.js
+++ js/index.js
@@ -16,11 +16,11 @@ var processed = {
     textfont: { 
         family: 'Courier New, monospace',  // Cambia el estilo de la fuente
         size: 15,                          // Ajusta el tamaño de la letra
-        color: '#0D0D0D' },        
-        /* color: 'rgb(82, 82, 82)', }, */
+        color: '#0D0D0D'
+    },
     mode: 'lines+markers+text',
-    marker: { size: 10,	opacity: 1, line: { width: 3 } },
-    line: {	color: 'rgb(182, 0, 0)', width: 4 }
+    marker: { size: 10, opacity: 1, line: { width: 3 } },
+    line: { color: 'rgb(182, 0, 0)', width: 4 }
 };
 
 // Configuración del diseño del gráfico
@@ -32,76 +32,53 @@ var layout = {
         title: "Año",
         titlefont: {
             family: 'Courier New Black',
-            size: 16,  // Tamaño del texto del título
-            color: '#4D1919',  // Color del texto del título
-            weight: 'bold'  // Estilo del texto (negrita)
+            size: 16,
+            color: '#4D1919',
+            weight: 'bold'
         },
-        showline: true,	showgrid: false, showticklabels: true,
-        linecolor: 'rgb(82, 82, 82)', linewidth: 4,	autotick: false,
-        tickfont: {	family: 'Courier New Black', size: 16, color: '#4D1919'	}
+        showline: false,
+        showgrid: false,
+        showticklabels: true,
+        linecolor: 'rgb(82, 82, 82)',
+        linewidth: 2,
+        autotick: false,
+        tickfont: {  // Aquí aseguramos que sea igual al eje Y
+            family: 'Courier New Black', // Mismo estilo
+            size: 16,                   // Mismo tamaño
+            color: '#4D1919'            // Mismo color
+        }
     },
 
     yaxis: {
-        title: 'Hectáreas quemadas (x 10^3)', // Título del eje Y
+        title: 'Hectáreas totales quemadas (x 10^3)', // Título del eje Y
         titlefont: {
-            family: 'Arial, sans-serif',
-            size: 14,
-            color: 'rgb(82, 82, 82)',
+            family: 'Courier New Black',
+            size: 16,
+            color: '#4D1919',
             weight: 'bold'
         },
-        showgrid: true,                // Muestra las líneas de la cuadrícula
-        zeroline: false,               // No muestra la línea en el cero
+        showgrid: true,
+        zeroline: false,
         showline: true,
-        linewidth: 1,
-        range: [-30, 630],             // Rango de los valores en el eje Y
-        showticklabels: true,          // Asegura que se muestren los números en el eje Y
+        linewidth: 0.5,
+        range: [-10, 630],
+        showticklabels: true,
         linecolor: 'rgb(82, 82, 82)',
-        tickfont: {
-            family: 'Arial',
-            size: 12,
-            color: 'rgb(82, 82, 82)'
+        tickfont: {  // Asegúrate de que el tickfont sea igual al del eje X
+            family: 'Courier New Black', // Mismo estilo
+            size: 16,                   // Mismo tamaño
+            color: '#4D1919'            // Mismo color
         },
-        dtick: 50,                     // Espaciado entre las graduaciones (cada 50 unidades)
-        tickvals: [0, 100, 200, 300, 400, 500, 600], // Opcional: valores específicos para los ticks
-        ticktext: ['0', '100', '200', '300', '400', '500', '600'] // Opcional: texto específico para cada tick
+        dtick: 50,
+        tickvals: [0, 100, 200, 300, 400, 500, 600],
+        ticktext: ['0', '100', '200', '300', '400', '500', '600']
     },
     margin: { autoexpand: false, l: 70, r: 100, t: 130 },
-    annotations: [{
-        xref: 'paper',
-        yref: 'paper',
-        x: -0.04,
-        y: 1.03,
-        textangle: -90,
-        xanchor: 'left',
-        yanchor: 'top',
-        text: 'Hectáreas totales quemadas (x 10^3)',
-        showarrow: false,
-        font: {
-            family: 'Arial',
-            size: 14,
-            color: 'rgb(100, 100, 100)'
-        }
-    }]
 };
-
-/*    yaxis: {
-        showgrid: false, zeroline: false, showline: true,
-        linewidth: 3, range: [-30, 630], showticklabels: false, linecolor: 'rgb(82, 82, 82)',
-        tickfont: { family: 'Courier New', size: 16, color: '#4D1919' }
-    },
-    margin: { autoexpand: false, l: 70, r: 100,	t: 130 },
-    
-    annotations: [{
-            xref: 'paper', yref: 'paper', x: -0.04, y: 1.1, textangle: -90, xanchor: 'left', yanchor: 'top',
-            text: '<b>Hectáreas totales quemadas (x10^3)</b>',
-            showarrow: false, font: { family: 'Courier New', size: 16, color: '#4D1919' },
-        }
-    ]
-}; */
 
 // Genera el gráfico
 Plotly.newPlot('myDiv', [processed], layout, {
     staticPlot: true,
     width: 1000,   // Ajusta el ancho del gráfico
-    height: 450   // Ajusta la altura del gráfico
+    height: 380    // Ajusta la altura del gráfico
 });
