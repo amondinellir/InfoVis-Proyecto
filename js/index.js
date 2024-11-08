d04bbe2156ba09da6c3d3f68e110ed9d850e5b56
amondinellir <amondinellir@uc.cl>
2024-11-08 12:29:31 -0300

    data in .js

--- js/index.js
+++ js/index.js
@@ -6,21 +6,21 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // Prepare demo data. The data is joined to map using the value of the 'hc-key'
     const data = [
-        { 'hc-key': 'cl-2730', value: 10, customInfo: 'IX' },
-        { 'hc-key': 'cl-bi', value: 11, customInfo: 'VIII' },
-        { 'hc-key': 'cl-ll', value: 12, customInfo: 'X' },
-        { 'hc-key': 'cl-li', value: 13, customInfo: 'VI' },
-        { 'hc-key': 'cl-ai', value: 14, customInfo: 'XI' },
-        { 'hc-key': 'cl-ma', value: 15, customInfo: 'XII' },
-        { 'hc-key': 'cl-co', value: 16, customInfo: 'IV' },
-        { 'hc-key': 'cl-at', value: 17, customInfo: 'III' },
-        { 'hc-key': 'cl-vs', value: 18, customInfo: 'V' },
-        { 'hc-key': 'cl-rm', value: 19, customInfo: 'RM' },
-        { 'hc-key': 'cl-ar', value: 20, customInfo: 'XIV' },
-        { 'hc-key': 'cl-ml', value: 21, customInfo: 'VII' },
-        { 'hc-key': 'cl-ta', value: 22, customInfo: 'I' },
-        { 'hc-key': 'cl-2740', value: 23, customInfo: 'XV' },
-        { 'hc-key': 'cl-an', value: 24, customInfo: 'II' }
+        { 'hc-key': 'cl-2730', value: 10, id: 'IX' },
+        { 'hc-key': 'cl-bi', value: 11, id: 'VIII' },
+        { 'hc-key': 'cl-ll', value: 12, id: 'X' },
+        { 'hc-key': 'cl-li', value: 13, id: 'VI' },
+        { 'hc-key': 'cl-ai', value: 14, id: 'XI' },
+        { 'hc-key': 'cl-ma', value: 15, id: 'XII' },
+        { 'hc-key': 'cl-co', value: 16, id: 'IV' },
+        { 'hc-key': 'cl-at', value: 17, id: 'III' },
+        { 'hc-key': 'cl-vs', value: 18, id: 'V' },
+        { 'hc-key': 'cl-rm', value: 19, id: 'RM' },
+        { 'hc-key': 'cl-ar', value: 20, id: 'XIV' },
+        { 'hc-key': 'cl-ml', value: 21, id: 'VII' },
+        { 'hc-key': 'cl-ta', value: 22, id: 'I' },
+        { 'hc-key': 'cl-2740', value: 23, id: 'XV' },
+        { 'hc-key': 'cl-an', value: 24, id: 'II' }
     ];
 
     // Create the map
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         credits: {enabled: false},
         colorAxis: {
             min: 0,
-            max: 252556.10,
+            max: 30, // Debe ser 252556.10
             stops: [
                 [0, '#FFFFFF'],   // White at the minimum value
                 [0.2, '#FFEDA0'], // Light yellow
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             },
             dataLabels: {
                 enabled: true,
-                format: '{point.customInfo}'
+                format: '{point.id}'
             }
         }],
         legend: {enabled: false}
